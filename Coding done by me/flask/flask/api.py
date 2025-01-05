ec2b66fd54ce4dba404e1f91b8ea8d1aa6d6c622
TheMadrasTechie <techiemadras@gmail.com>
2025-01-05 22:08:10 +0530

    simply adding to maintain the github streak

--- Coding done by me/flask/flask/api.py
+++ Coding done by me/flask/flask/api.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 ##Initial Data in my to do list
-items = [
+items = [ 
     {"id": 1, "name": "Item 1", "description": "This is item 1"},
     {"id": 2, "name": "Item 2", "description": "This is item 2"},
     {"id": 3, "name": "Item 3", "description": "This is item 3"},
